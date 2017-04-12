94ca67cd93d39d7feb459e63df1df38110bf4093
dm00474408 <dm00474408@INPUHJPC09773.ATT.TechMahindra.com>
2017-04-12 20:39:12 +0530

    Commit1

---
+++ Sample/src/git_sample/Git_class.java
@@ -0,0 +1,12 @@
+package git_sample;
+
+public class Git_class {
+
+	
+	public static void main(String[] args) {
+     
+		System.out.println("Hello World!");
+		System.out.println("Hello World1!");
+	}
+
+}
